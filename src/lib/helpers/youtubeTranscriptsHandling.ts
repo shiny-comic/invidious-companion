56a73ff3a3e6ee158451bdfda19d562c7004aace
shiny-comic <shiny-comic@winPC>
2026-01-07 18:09:00 +0900

    Adjust indent to 4

--- src/lib/helpers/youtubeTranscriptsHandling.ts
+++ src/lib/helpers/youtubeTranscriptsHandling.ts
@@ -51,11 +51,21 @@ export async function handleTranscripts(
 ) {
     if (poToken && clientName) {
         const baseUrl = selectedCaption.base_url;
+<<<<<<< HEAD
         let url = `${baseUrl}&fmt=vtt&potc=1&pot=${poToken}&c=${clientName}`;
 
         const urlObj = new URL(url);
 
         const response = await innertubeClient.session.http.fetch(urlObj, { method: "GET" });
+=======
+        const url = `${baseUrl}&fmt=vtt&potc=1&pot=${poToken}&c=${clientName}`;
+
+        const urlObj = new URL(url);
+
+        const response = await innertubeClient.session.http.fetch(urlObj, {
+            method: "GET",
+        });
+>>>>>>> b21d071 (Adjust indent to 4)
 
         if (!response.ok) {
             throw new HTTPException(response.status as ContentfulStatusCode, {
@@ -79,6 +89,7 @@ export async function handleTranscripts(
 
         const info = await innertubeClient.getInfo(videoId);
         const transcriptInfo = await (await info.getTranscript())
+<<<<<<< HEAD
         .selectLanguage(
             selectedCaption.name.text || "",
         );
@@ -110,12 +121,46 @@ export async function handleTranscripts(
                 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DurationFormat/DurationFormat
 
                 const start_ms = createTemporalDuration(Number(line.start_ms))
+=======
+            .selectLanguage(
+                selectedCaption.name.text || "",
+            );
+        const rawTranscriptLines = transcriptInfo.transcript.content?.body
+            ?.initial_segments;
+
+        if (rawTranscriptLines == undefined) throw new HTTPException(404);
+
+        rawTranscriptLines.forEach((line) => {
+            const timestampFormatOptions = {
+                style: "digital",
+                minutesDisplay: "always",
+                fractionalDigits: 3,
+            };
+
+            // Temporal.Duration.prototype.toLocaleString() is supposed to delegate to Intl.DurationFormat
+            // which Deno does not support. However, instead of following specs and having toLocaleString return
+            // the same toString() it seems to have its own implementation of Intl.DurationFormat,
+            // with its options parameter type incorrectly restricted to the same as the one for Intl.DateTimeFormatOptions
+            // even though they do not share the same arguments.
+            //
+            // The above matches the options parameter of Intl.DurationFormat, and the resulting output is as expected.
+            // Until this is fixed typechecking must be disabled for the two use cases below
+            //
+            // See
+            // https://docs.deno.com/api/web/~/Intl.DateTimeFormatOptions
+            // https://docs.deno.com/api/web/~/Temporal.Duration.prototype.toLocaleString
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Temporal/Duration/toLocaleString
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DurationFormat/DurationFormat
+
+            const start_ms = createTemporalDuration(Number(line.start_ms))
+>>>>>>> b21d071 (Adjust indent to 4)
                 .round({
                     largestUnit: "year",
                     relativeTo: Temporal.PlainDateTime.from("2022-01-01"),
                     //@ts-ignore see above
                 }).toLocaleString("en-US", timestampFormatOptions);
 
+<<<<<<< HEAD
                 const end_ms = createTemporalDuration(Number(line.end_ms)).round({
                     largestUnit: "year",
                     relativeTo: Temporal.PlainDateTime.from("2022-01-01"),
@@ -135,5 +180,26 @@ export async function handleTranscripts(
             });
 
             return lines.join("\n\n");
+=======
+            const end_ms = createTemporalDuration(Number(line.end_ms)).round({
+                largestUnit: "year",
+                relativeTo: Temporal.PlainDateTime.from("2022-01-01"),
+                //@ts-ignore see above
+            }).toLocaleString("en-US", timestampFormatOptions);
+            const timestamp = `${start_ms} --> ${end_ms}`;
+
+            const text = (line.snippet?.text || "").replace(
+                /[&<>‍‍\u200E\u200F\u00A0]/g,
+                (match: string) =>
+                    ESCAPE_SUBSTITUTIONS[
+                        match as keyof typeof ESCAPE_SUBSTITUTIONS
+                    ],
+            );
+
+            lines.push(`${timestamp}\n${text}`);
+        });
+
+        return lines.join("\n\n");
+>>>>>>> b21d071 (Adjust indent to 4)
     }
 }
